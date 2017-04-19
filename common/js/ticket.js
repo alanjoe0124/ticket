e39babd099d04f6e590e88db3485b107303a3e8b
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-19 16:00:39 +0800

    Create ticket form

---
+++ common/js/ticket.js
@@ -0,0 +1,25 @@
+
+function create_ticket(){
+    $(".create_ticket").click(function () {
+        var form = '<form class = "ticket_form"><p>ticket</p>' +
+                   '<p>title</p><input type="text" name="title">' +
+                   '<br>desc<br><textarea rows="10" cols="20"></textarea>' +
+                   '<br><button>submit</button></form>';
+        $(".ticket").append(form);
+    });
+    $('button').on('click', function () {
+       $('.ticket_form').on('submit', function () {
+            var title = $('input[name=title]').val(), description = $('textarea').val(), email = $('#email').html();
+                    $.post( "http://ticket.dev/ticket_create.php", 
+                    { title: title, description: description, email:email  }
+                    ).done( function( data ) {
+                       // alert( "Data Loaded: " + title + description +email);
+                    });
+       });
+    });
+}
+
+$(document).ready(function () {
+   create_ticket();
+});
+
