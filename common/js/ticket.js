f712c2e475d8ce797f92dabca3fff030f3bd3888
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-24 14:46:45 +0800

    fix(ticket create):submit params won't show on URL, submit event won't dup

--- common/js/ticket.js
+++ common/js/ticket.js
@@ -1,14 +1,27 @@
+function form_generate() {
+    var form = '<form class="ticket_form" style="display:none">' +
+            '<p>ticket</p>' +
+            '<p>title</p>' +
+            '<input type="text" name="ticket_title">' +
+            '<br>desc<br>' +
+            '<textarea id="descprition" rows="10" cols="20"></textarea>' +
+            '<br><button type="submit" id="ticket_sub">submit</button>' +
+            '</form>';
+    $(".ticket").append(form);
+}
 
 function create_ticket() {
-    $(".create_ticket").click(function () { 
-        $('.ticket').show();
-        $("#ticket_sub").click(function () {
-            $('.ticket_form').submit(function () {
-                alert('send successfully');
-                var title = $('input[name=ticket_title]').val(), description = $('#descprition').val(), email = $('#email').text();
-                $.post("http://ticket.dev/ticket_create.php", {title: title, description: description, email: email});
-            });
-        });
+    $(".create_ticket").click(function () {
+        $('.ticket_form').css("display", "inline");
+    });
+}
+
+function submit_ticket() {
+    $(".ticket_form").submit(function (event) {
+        alert('send successfully');
+        var title = $('input[name=ticket_title]').val(), description = $('#descprition').val(), email = $('#email').text();
+        $.post("http://ticket.dev/ticket_create.php", {title: title, description: description, email: email});
+        event.preventDefault();
     });
 }
 
@@ -16,25 +29,14 @@ function view_ticket() {
     var email = $('#email').text();
     $(".view_ticket").click(function () {
         url = "http://ticket.dev/index.php?email=" + email;
-        $(location).attr("href", url);
+        location.href=url;
     });
 }
 
-function form_generate(){
-     var form = '<form class="ticket_form">' +
-                        '<p>ticket</p>' +
-                        '<p>title</p>'+
-                        '<input type="text" name="ticket_title">' +
-                        '<br>desc<br>'+
-                        '<textarea id="descprition" rows="10" cols="20"></textarea>' +
-                        '<br><button type="submit" id="ticket_sub">submit</button>'+
-                '</form>';
-        $(".ticket").append(form);
-        $('.ticket').hide();
-}
 $(document).ready(function () {
     form_generate();
     create_ticket();
+    submit_ticket();
     view_ticket();
 });
 
