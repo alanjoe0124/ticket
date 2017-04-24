e51606146ba0662d8febaf875094ba8f24cf1ac1
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-24 10:13:27 +0800

    fix(prevent_csrf): reduce the no need code

--- prevent_csrf.php
+++ prevent_csrf.php
@@ -3,15 +3,12 @@ try {
     if (!isset($_SERVER['HTTP_REFERER'])) {
         throw new InvalidArgumentException('Missing HTTP REFERER');
     }
-    $referer = preg_match('#^http://([^/]+)#', $_SERVER['HTTP_REFERER'], $match);
-    $domainName = $match[1];
-    if (strlen($domainName) > 70) {
-        throw new InvalidArgumentException('Domain name invalid');
-    }
+    $referer = preg_match('#^http://([^/]+)#', $_SERVER['HTTP_REFERER'], $matches);
+    $domainName = $matches[1]; 
     if ($domainName != 'ourblog.dev' && $domainName != 'ticket.dev') {
         throw new InvalidArgumentException('SERVER_NAME and HTTP_REFERER mismatch');
     }
 } catch (Exception $e) {
-    exit();
+    exit;
 }
  
