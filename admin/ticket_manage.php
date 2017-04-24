76fbaae9b71516ff010cebb7a91878ac0dc377ba
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-24 16:30:32 +0800

    style(ticket_manage):update the code style

--- admin/ticket_manage.php
+++ admin/ticket_manage.php
@@ -2,6 +2,7 @@
 session_start();
 if (!isset($_SESSION['uid'])) {
     header('Location:/admin/login.php');
+    exit;
 }
 ?>
 <html>
@@ -17,7 +18,7 @@ if (!isset($_SESSION['uid'])) {
                 <div class="head-main-box">
                     <div class="head-title">
                         <p><h1>Ticket</h1>
-                        <p>user: <?php echo $_SESSION['userName']; ?>&nbsp;/&nbsp;<a href="/admin/logout.php">logout</a></p>
+                        <p>user: <?php echo htmlspecialchars($_SESSION['userName']); ?>&nbsp;/&nbsp;<a href="/admin/logout.php">logout</a></p>
                     </div>
                     <HR width="100%">
                 </div>
@@ -34,39 +35,42 @@ if (!isset($_SESSION['uid'])) {
                                 ticket.title, 
                                 status.name AS status, 
                                 ticket.user as customer, 
-                                ticket.http_referer, 
+                                ticket.domain, 
                                 ticket.time 
                         FROM    ticket 
                                 INNER JOIN status ON ticket.status = status.id 
                         WHERE 
                                 status = 1    
                                 ORDER BY time ASC";  // ticket status ( 1 => pending, 2 => close ) 
-                $stmt = $db->query($sql); 
+                $stmt = $db->query($sql);
                 $ticketRows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                if ($ticketRows) {
-                    echo '<table>
+                if ($ticketRows) :
+                    ?>
+                    <table>
+                        <tr>
+                            <th>title</th>
+                            <th>customer</th>
+                            <th>status</th>
+                            <th>time</th>
+                            <th>httpreferer</th>
+                        </tr>
+                        <?php
+                        foreach ($ticketRows as $row):
+                            ?>
                             <tr>
-                                <th>title</th>
-                                <th>customer</th>
-                                <th>status</th>
-                                <th>time</th>
-                                <th>httpreferer</th>
-                            </tr>';
-                    foreach ($ticketRows as $ticketRow) {
-                        if (mb_strlen($ticketRow['title'], 'UTF-8') > 20) {
-                            $ticketRow['title'] = mb_substr($ticketRow['title'], 0, 20, 'UTF-8')."...";
-                        }
-                        echo '<tr>
-                                  <td><a href = "/admin/ticket_answer.php?ticket='.$ticketRow['id'].'">'
-                                       .htmlspecialchars($ticketRow['title']).'</a></td>
-                                  <td>'.htmlspecialchars($ticketRow['customer']).'</td>
-                                  <td>'.$ticketRow['status'].'</td>
-                                  <td>'.$ticketRow['time'].'</td>
-                                  <td>'.$ticketRow['http_referer'].'</td>
-                              </tr>';
-                    }
-                    echo "</table>";
-                }
+                                <td><a href = "/admin/ticket_answer.php?ticket=<?php echo $row['id']; ?> ">
+                                        <?php echo htmlspecialchars($row['title']); ?></a></td>
+                                <td><?php echo htmlspecialchars($row['customer']); ?></td>
+                                <td><?php echo $row['status']; ?></td>
+                                <td><?php echo $row['time']; ?></td>
+                                <td><?php echo htmlspecialchars($row['domain']); ?></td>
+                            </tr>
+                            <?php
+                        endforeach;
+                        ?>
+                    </table>
+                    <?php
+                endif;
                 ?>
             </div>
             <div class="sidebox"></div>
