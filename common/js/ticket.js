104b831f879b6402340e5d58ca7040c95e535e25
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-25 14:32:51 +0800

    fix(ticket.js): reduce no need variable and function

--- common/js/ticket.js
+++ common/js/ticket.js
@@ -1,44 +1,29 @@
-function form_generate() {
-    var form = '<form class="ticket_form" style="display:none">' +
-            '<p>ticket</p>' +
-            '<p>title</p>' +
-            '<input type="text" name="ticket_title">' +
-            '<br>desc<br>' +
-            '<textarea id="descprition" rows="10" cols="20"></textarea>' +
-            '<br><button type="submit" id="ticket_sub">submit</button>' +
-            '</form>';
-    $(".ticket").append(form);
-}
 
-function create_ticket() {
+$(document).ready(function () {
+    var form = '<form class="ticket_form" style="display:none">' +
+                        '<p>ticket</p>' +
+                        '<p>title</p>' +
+                    '<input type="text" name="ticket_title">' +
+                        '<br>desc<br>' +
+                    '<textarea id="descprition" rows="10" cols="20"></textarea>' +
+                    '<br><button type="submit" id="ticket_sub">submit</button>' +
+                '</form>';
+    $(".ticket").append(form);
+    
     $(".create_ticket").click(function () {
         $('.ticket_form').css("display", "inline");
     });
-}
-
-function submit_ticket() {
-    $(".ticket_form").submit(function (event) {
-        var title = $('input[name=ticket_title]').val(), description = $('#descprition').val(), email = $('#email').text();
+    
+    $(".ticket_form").submit(function (e) {
+        e.preventDefault(); 
         $.post("http://ticket.dev/ticket_create.php", 
-                {title: title, description: description, email: email, domain:location.host}, 
+                {title: $('input[name=ticket_title]').val(), description: $('#descprition').val(), email: $('#email').text(), domain:location.host}, 
                 function(){ alert('send successfully')}
-                );
-        event.preventDefault();  
+                );    
     });
-}
-
-function view_ticket() {
-    var email = $('#email').text();
+    
     $(".view_ticket").click(function () {
-        url = "http://ticket.dev/index.php?email=" + email;
-        location.href=url;
+        location.href="http://ticket.dev/index.php?email=" +  $('#email').text();
     });
-}
-
-$(document).ready(function () {
-    form_generate();
-    create_ticket();
-    submit_ticket();
-    view_ticket();
 });
 
