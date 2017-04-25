8a2a96c7eb6059f1fe521889217c49406926ae45
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-25 17:38:48 +0800

    fix(login):reduce some no need code

--- admin/login.php
+++ admin/login.php
@@ -1,14 +1,10 @@
 <?php
 session_start();
 if (isset($_SESSION['uid'])) {
-    header("Location:/admin/ticket_manage.php");
+    header('Location:/admin/ticket_manage.php');
     exit;
 };
-if (isset($_SESSION['customerEmail'])){
-    $customerEmail = $_SESSION['customerEmail'];
-    header("Location:/index.php?email=$customerEmail");
-    exit;
-}
+
 if ($_POST) {
     try {
         require_once __DIR__. '/../prevent_csrf.php';
@@ -21,17 +17,17 @@ if ($_POST) {
         $_POST['userName']=trim($_POST['userName']);
         $userNameLength = mb_strlen($_POST['userName'], 'UTF-8');
         if ( $userNameLength > 50 || $userNameLength < 3 ) {
-            throw new InvalidArgumentException("User name max length 50, min length 3");
+            throw new InvalidArgumentException('User name max length 50, min length 3');
         }
         $pwdLength = strlen($_POST['pwd']);
         if ($pwdLength > 40 || $pwdLength < 5 ) {
-            throw new InvalidArgumentException("Password max length 40, min length 5");
+            throw new InvalidArgumentException('Password max length 40, min length 5');
         }
     } catch (Exception $e) {
         exit("Param error!");
     }
     require_once __DIR__ . '/../db.php';
-    $sql = "SELECT id, name FROM user WHERE name = ? and pwd = ?";
+    $sql = 'SELECT id, name FROM user WHERE name = ? and pwd = ?';
     $stmt = $db->prepare($sql);
     $salt = 'acd806b0-d563-4824-907f-852f8f1003a5'; 
     $stmt->execute(array($_POST['userName'], md5($_POST['pwd'].$salt)));
@@ -40,10 +36,10 @@ if ($_POST) {
         session_regenerate_id();
         $_SESSION['uid'] = $userRow['id'];
         $_SESSION['userName'] = $userRow['name'];
-        header("Location:/admin/ticket_manage.php");
+        header('Location:/admin/ticket_manage.php');
         exit;
     } else {
-        header("Location:/admin/login.php?error=password_failed");
+        header('Location:/admin/login.php?error=password_failed');
         exit;
     }
 }
