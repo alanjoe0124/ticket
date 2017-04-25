8d68ad4a7072af4f8fc9d37cef61af1d04747fe6
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-25 15:29:59 +0800

    fix(close.php):move the href for close ticket from close.php to ticket_info.php, and reduce no need code in close.php

--- close.php
+++ close.php
@@ -1,52 +1,34 @@
-<?php
-if (isset($_GET['action'])) {
-    session_start();
-    try {
-        require_once __DIR__ . '/prevent_csrf.php';
-        if (!isset($_SESSION['customerEmail'])) {
-            throw new InvalidArgumentException("Permission denied");
-        }
-        if (!isset($_GET['action'])) {
-            throw new InvalidArgumentException("Missing required action");
-        }
-        if ($_GET['action'] != 'close') {
-            throw new InvalidArgumentException("Action Invalid");
-        }
-        if (!isset($_GET['ticket'])) {
-            throw new InvalidArgumentException("Missing required ticket");
-        }
-        $ticketId = filter_var($_GET['ticket'], FILTER_VALIDATE_INT, array(
-            'options' => array('min_range' => 1)
-        ));
-    } catch (Exception $e) {
-        exit($e->getMessage());
+<?php 
+session_start();
+try {
+    require_once __DIR__ . '/prevent_csrf.php';
+    if (!isset($_SESSION['customerEmail'])) {
+        throw new InvalidArgumentException("Permission denied");
     }
+    if (!isset($_GET['ticket'])) {
+        throw new InvalidArgumentException("Missing required ticket");
+    }
+    $ticketId = filter_var($_GET['ticket'], FILTER_VALIDATE_INT, array(
+        'options' => array('min_range' => 1)
+    ));
+} catch (Exception $e) {
+    exit($e->getMessage());
+}
 
-    require_once __DIR__ . '/db.php';
-    $sql = "SELECT ticket.status
+require_once __DIR__ . '/db.php';
+$sql = "SELECT ticket.status
         FROM ticket 
             INNER JOIN customer ON ticket.user = customer.id
         WHERE 
             customer.name = ? AND ticket.id = $ticketId";
-    $stmt = $db->prepare($sql);
-    $stmt->execute(array($_SESSION['customerEmail']));
-    $status = $stmt->fetchColumn();
-    if (!$status) {
-        exit('No authority to operate this ticket');
-    }
-    if ($status != 2) { // ticket status ( 1 => pending, 2 => close ) 
-        $sql = $db->exec("UPDATE ticket SET status = 2 WHERE id = $ticketId");
-    }
-    header("Location:/ticket_ask.php?ticket=$ticketId");
-    exit;
+$stmt = $db->prepare($sql);
+$stmt->execute(array($_SESSION['customerEmail']));
+$status = $stmt->fetchColumn();
+if (!$status) {
+    exit('No authority to operate this ticket');
 }
-if (isset($ticketId)):
-    if(isset($_SESSION['customerEmail'])):
-        if ($ticketRow['status'] == "pending") :
-            ?>
-            <a href="/close.php?ticket=<?php echo $ticketId; ?>&action=close"><button>close</button></a>
-            <?php
-        endif;
-    endif;
-endif;
- 
+if ($status != 2) { // ticket status ( 1 => pending, 2 => close ) 
+    $sql = $db->exec("UPDATE ticket SET status = 2 WHERE id = $ticketId");
+}
+header("Location:/ticket_ask.php?ticket=$ticketId");
+exit;
--- common/ticket_info.php
+++ common/ticket_info.php
@@ -1,6 +1,6 @@
 <?php
 if (!isset($ticketId)) {
-    exit("Permission denied");
+    exit("Missing required ticket");
 }
 
 $sql = "SELECT customer.name AS customer, 
@@ -15,31 +15,30 @@ $sql = "SELECT customer.name AS customer,
                                ticket.id = $ticketId";
 $stmt = $db->query($sql);
 $ticketRow = $stmt->fetch(PDO::FETCH_ASSOC);
-if ($ticketRow) :
-    ?>
-    <div>ticket info:
-        <?php
-        include __DIR__. '/../close.php';
-        ?>
-    </div><HR width="100%">
-    <div>
-        <table>
-            <tr>
-                <th>title</th>
-                <th>description</th>
-                <th>customer</th>
-                <th>status</th>
-                <th>time</th>
-            </tr>
-            <tr>
-                <td><?php echo htmlspecialchars($ticketRow['title']); ?></td>
-                <td><?php echo htmlspecialchars($ticketRow['description']); ?></td>
-                <td><?php echo htmlspecialchars($ticketRow['customer']); ?></td>
-                <td><?php echo $ticketRow['status']; ?></td>
-                <td><?php echo $ticketRow['time']; ?></td>       
-            </tr>
-        </table>
-    </div>
-    <?php
-endif;
 ?>
+<div>ticket info:
+    <?php if (isset($_SESSION['customerEmail'])): ?>
+        <?php if ($ticketRow['status'] == "pending"): ?>
+            <a href="/close.php?ticket=<?php echo $ticketId; ?>"><button>close</button></a>
+        <?php endif; ?>
+    <?php endif; ?>
+</div>
+<HR width="100%">
+<div>
+    <table>
+        <tr>
+            <th>title</th>
+            <th>description</th>
+            <th>customer</th>
+            <th>status</th>
+            <th>time</th>
+        </tr>
+        <tr>
+            <td><?php echo htmlspecialchars($ticketRow['title']); ?></td>
+            <td><?php echo htmlspecialchars($ticketRow['description']); ?></td>
+            <td><?php echo htmlspecialchars($ticketRow['customer']); ?></td>
+            <td><?php echo $ticketRow['status']; ?></td>
+            <td><?php echo $ticketRow['time']; ?></td>       
+        </tr>
+    </table>
+</div>
