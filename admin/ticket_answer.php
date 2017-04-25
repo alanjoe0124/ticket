04757c82f39ad46807fd6ddc94f95fefb3dea180
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-25 16:23:52 +0800

    fix(ticket_manage & ticket_answer): separate head and footer, add one more condition for sql order by

--- admin/ticket_answer.php
+++ admin/ticket_answer.php
@@ -12,18 +12,18 @@ if ($_POST) {
         }
         $commentLength = strlen($_POST['comment']);
         if ($commentLength > 64000 || $commentLength == 0) {
-            throw new InvalidArgumentException("Comment max length 64000 and not empty");
+            throw new InvalidArgumentException('Comment max length 64000 and not empty');
         }
         $ticketId = filter_var($_POST['ticketId'], FILTER_VALIDATE_INT, array(
             'options' => array('min_range' => 1)
         ));
         if (!$ticketId) {
-            throw new InvalidArgumentException("Invalid ticket id");
+            throw new InvalidArgumentException('Invalid ticket id');
         }
     } catch (Exception $e) {
         exit('Form Param error');
     }
-    $sql = "INSERT INTO comment (content, user, ticket_id, user_type) VALUES (?, ?, ?, ?)";
+    $sql = 'INSERT INTO comment (content, user, ticket_id, user_type) VALUES (?, ?, ?, ?)';
     $stmt = $db->prepare($sql);
     $stmt->execute(array($_POST['comment'], $_SESSION['uid'], $ticketId, 2)); // user_type ( 1 = > table(`customer`) , 2 => table (`user`)
     header("Location:/admin/ticket_answer.php?ticket=$ticketId");
@@ -32,52 +32,34 @@ if ($_POST) {
 
 try {
     if (!isset($_SESSION['uid'])) {
-        throw new InvalidArgumentException("Permission denied");
+        throw new InvalidArgumentException('Permission denied');
     }
     if (!isset($_GET['ticket'])) {
-        throw new InvalidArgumentException("Missing required ticket id");
+        throw new InvalidArgumentException('Missing required ticket id');
     }
     $ticketId = filter_var($_GET['ticket'], FILTER_VALIDATE_INT, array(
         'options' => array('min_range' => 1)
     ));
     if (!$ticketId) {
-        throw new InvalidArgumentException("Invalid ticket id");
+        throw new InvalidArgumentException('Invalid ticket id');
     }
 } catch (Exception $e) {
     exit($e->getMessage());
 }
 ?>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="/common/css/main.css">
-    </head>
-    <body>
-        <div class="container">
-            <!--content_head start-->
-            <div class="headbox">
-                <div class="head-side-box"></div>
-                <div class="head-main-box">
-                    <div class="head-title">    
-                        <a href="/admin/ticket_manage.php"><h1>Ticket</h1></a>
-                        user:<?php echo $_SESSION['userName']; ?> / <a href="/admin/logout.php">logout</a></p>
-                    </div>
-                    <HR width="100%">
-                </div>
-                <div class="head-side-box"></div>
-            </div> 
-            <!--content_head end->
-            
-            <!--contetn_body start-->
-            <div class="sidebox"> </div>
-            <div class="mainbox">
-                <?php
-                include __DIR__ . '/../common/ticket_info.php';
-                include __DIR__ . '/../common/ticket_comments.php';
-                include __DIR__ . '/../common/ticket_comment_form.php';
-                ?>
-            </div>
-    </body>
+<?php include __DIR__ . '/admin_head.php'; ?>
+<!--content_head end->
+
+<!--contetn_body start-->
+<div class="sidebox"> </div>
+<div class="mainbox">
+    <?php
+    include __DIR__ . '/../common/ticket_info.php';
+    include __DIR__ . '/../common/ticket_comments.php';
+    include __DIR__ . '/../common/ticket_comment_form.php';
+    ?>
+</div>
+</body>
 </html>
 
 
--- admin/ticket_manage.php
+++ admin/ticket_manage.php
@@ -5,76 +5,50 @@ if (!isset($_SESSION['uid'])) {
     exit;
 }
 ?>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="/../common/css/main.css">
-    </head>
-    <body>
-        <div class="container">
-            <!--content_head start-->
-            <div class="headbox">
-                <div class="head-side-box"></div>
-                <div class="head-main-box">
-                    <div class="head-title">
-                        <p><h1>Ticket</h1>
-                        <p>user: <?php echo htmlspecialchars($_SESSION['userName']); ?>&nbsp;/&nbsp;<a href="/admin/logout.php">logout</a></p>
-                    </div>
-                    <HR width="100%">
-                </div>
-                <div class="head-side-box"></div>
-            </div> 
-            <!--content_head end->
-            
-            <!--contetn_body start-->
-            <div class="sidebox"> </div>
-            <div class="mainbox">
-                <?php
-                require_once __DIR__ . '/../db.php';
-                $sql = "SELECT  ticket.id AS id,
-                                ticket.title, 
-                                status.name AS status, 
+<!--content_head start-->
+<?php include __DIR__ . '/admin_head.php'; ?>
+<!--content_head end->
+
+<!--contetn_body start-->
+<div class="sidebox"> </div>
+<div class="mainbox">
+    <?php
+    require_once __DIR__ . '/../db.php';
+    $sql = 'SELECT  status.name AS status,
+                                ticket.id AS id,
+                                ticket.title,
                                 ticket.user as customer, 
                                 ticket.domain, 
-                                ticket.time 
+                                ticket.time,
+                                customer.name as customer
                         FROM    ticket 
-                                INNER JOIN status ON ticket.status = status.id 
+                                INNER JOIN status ON status.id = ticket.status
+                                INNER JOIN customer ON ticket.user = customer.id
                         WHERE 
                                 status = 1    
-                                ORDER BY time ASC";  // ticket status ( 1 => pending, 2 => close ) 
-                $stmt = $db->query($sql);
-                $ticketRows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                if ($ticketRows) :
-                    ?>
-                    <table>
-                        <tr>
-                            <th>title</th>
-                            <th>customer</th>
-                            <th>status</th>
-                            <th>time</th>
-                            <th>httpreferer</th>
-                        </tr>
-                        <?php
-                        foreach ($ticketRows as $row):
-                            ?>
-                            <tr>
-                                <td><a href = "/admin/ticket_answer.php?ticket=<?php echo $row['id']; ?> ">
-                                        <?php echo htmlspecialchars($row['title']); ?></a></td>
-                                <td><?php echo htmlspecialchars($row['customer']); ?></td>
-                                <td><?php echo $row['status']; ?></td>
-                                <td><?php echo $row['time']; ?></td>
-                                <td><?php echo htmlspecialchars($row['domain']); ?></td>
-                            </tr>
-                            <?php
-                        endforeach;
-                        ?>
-                    </table>
-                    <?php
-                endif;
-                ?>
-            </div>
-            <div class="sidebox"></div>
-            <!--contetn_body end-->
-        </div>
-    </body>
-</html>
+                                ORDER BY time DESC, ticket.id DESC';  // ticket status ( 1 => pending, 2 => close ) 
+    $stmt = $db->query($sql);
+    $ticketRows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    ?> 
+    <table>
+        <tr>
+            <th>title</th>
+            <th>customer</th>
+            <th>status</th>
+            <th>time</th>
+            <th>domain</th>
+        </tr>
+        <?php foreach ($ticketRows as $row): ?>
+            <tr>
+                <td><a href = "/admin/ticket_answer.php?ticket=<?php echo $row['id']; ?> ">
+                        <?php echo htmlspecialchars($row['title']); ?></a></td>
+                <td><?php echo htmlspecialchars($row['customer']); ?></td>
+                <td><?php echo $row['status']; ?></td>
+                <td><?php echo $row['time']; ?></td>
+                <td><?php echo htmlspecialchars($row['domain']); ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table> 
+</div> 
+<!--contetn_body end-->
+<?php include __DIR__ . '/admin_footer.php'; ?>
