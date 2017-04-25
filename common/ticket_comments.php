58b7b15a3cf5527faead8dc6d79819bf4e8ccdb2
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-25 17:37:36 +0800

    fix(ticket_comments): modify method to query comments info

--- common/ticket_comments.php
+++ common/ticket_comments.php
@@ -1,60 +1,52 @@
 <?php
 if (!isset($ticketId)) {
-    exit("Permission denied");
+    exit('Missing required ticket');
 }
-
 // user_type ( 1 = > table(`customer`) , 2 => table (`user`)
-$sql = "SELECT comment.content,
-               comment.time,
-               user.name as user
-            FROM comment
-                    INNER JOIN user ON user.id = comment.user
-            WHERE comment.ticket_id = $ticketId AND comment.user_type = 2 ORDER BY time ASC";
-$stmt = $db->query($sql);
-$csUserCommentRows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$comments = array();
-foreach ($csUserCommentRows as $row) {
-    $comments[$row['time']] = array('content' => $row['content'], 'user' => $row['user']);
-}
-$sql = "SELECT comment.content,
-               comment.time,
-               customer.name as user
-            FROM comment
-                    INNER JOIN customer ON  comment.user = customer.id
-            WHERE comment.ticket_id = $ticketId AND comment.user_type = 1 ORDER BY time ASC";
-$stmt = $db->query($sql);
-$customerCommentRows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach ($customerCommentRows as $row) {
-    $comments[$row['time']] = array('content' => $row['content'], 'user' => $row['user']);
-}
-ksort($comments);
-?>
 
+$sql = "SELECT comment.content,
+               comment.time,
+               user,
+               user_type
+            FROM comment 
+            WHERE comment.ticket_id = $ticketId ORDER BY time DESC, id DESC";
+$stmt = $db->query($sql);
+$comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?> 
 <HR width="100%">
-<div>comments:</div>
-<?php
-if (!empty($comments)):
-    foreach ($comments as $time => $row):
-        ?>
-        <HR width="100%">
-        <div>
-            <table>
-                <tr>
-                    <th>user</th>
-                    <th>comment</th>
-                    <th>time</th>
-                </tr>
-                <tr>
-                    <td><?php echo htmlspecialchars($row['user']); ?></td>
-                    <td><?php echo htmlspecialchars($row['content']); ?></td>
-                    <td><?php echo $time; ?></td>
-                </tr>
-            </table>
-        </div>
-        <?php
-    endforeach;
-endif;
-?>
+<div>comments:</div> 
+<?php foreach ($comments as $row): ?>
+    <HR width="100%">
+    <div>
+        <table>
+            <tr>
+                <th>user</th>
+                <th>comment</th>
+                <th>time</th>
+            </tr>
+            <tr>
+                <td>
+                    <?php
+                    if ($row['user_type'] == 1) {
+                        $table = 'customer';
+                    } else {                      // user_type = 2
+                        $table = 'user';
+                    }
+                    $sql = 'SELECT name FROM ' . $table . ' WHERE id = ' . $row['user'];
+                    $stmt = $db->query($sql);
+                    $name = $stmt->fetchColumn();
+                    echo htmlspecialchars($name);
+                    ?>
+                </td>
+                <td><?php echo htmlspecialchars($row['content']); ?></td>
+                <td><?php echo $row['time']; ?></td>
+            </tr>
+        </table>
+    </div>
+<?php endforeach; ?>
+    
+
+
 
 
 
