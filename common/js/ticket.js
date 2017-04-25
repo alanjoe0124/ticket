35d5085787aa1218761daef2961057f5bf87b584
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-25 16:47:23 +0800

    fix(ticket.js): check if data sent successfully

--- common/js/ticket.js
+++ common/js/ticket.js
@@ -1,29 +1,33 @@
 
 $(document).ready(function () {
     var form = '<form class="ticket_form" style="display:none">' +
-                        '<p>ticket</p>' +
-                        '<p>title</p>' +
-                    '<input type="text" name="ticket_title">' +
-                        '<br>desc<br>' +
-                    '<textarea id="descprition" rows="10" cols="20"></textarea>' +
-                    '<br><button type="submit" id="ticket_sub">submit</button>' +
-                '</form>';
+            '<p>ticket</p>' +
+            '<p>title</p>' +
+            '<input type="text" name="ticket_title">' +
+            '<br>desc<br>' +
+            '<textarea id="descprition" rows="10" cols="20"></textarea>' +
+            '<br><button type="submit" id="ticket_sub">submit</button>' +
+            '</form>';
     $('.ticket').append(form);
-    
+
     $('.create_ticket').click(function () {
         $('.ticket_form').css('display', 'inline');
     });
-    
+
     $('.ticket_form').submit(function (e) {
-        e.preventDefault(); 
-        $.post('http://ticket.dev/ticket_create.php', 
-                {title: $('input[name=ticket_title]').val(), description: $('#descprition').val(), email: $('#email').text(), domain:location.host}, 
-                function(){ alert('send successfully')}
-                );    
+        e.preventDefault();
+        $.post('http://ticket.dev/ticket_create.php',
+                {title: $('input[name=ticket_title]').val(), description: $('#descprition').val(), email: $('#email').text(), domain: location.host},
+                function (response) {
+                    if (response === "success") {
+                        alert('send successfully');
+                    }
+                },
+                "text");
     });
-    
+
     $('.view_ticket').click(function () {
-        location.href='http://ticket.dev/index.php?email=' +  $('#email').text();
+        location.href = 'http://ticket.dev/index.php?email=' + $('#email').text();
     });
 });
 
--- ticket_create.php
+++ ticket_create.php
@@ -11,41 +11,42 @@ try {
     }
     $titleLength = mb_strlen($_POST['title'], "utf-8");
     if ($titleLength > 500 || $titleLength < 1) {
-        throw new InvalidArgumentException("Title max length 500, min length 1");
+        throw new InvalidArgumentException('Title max length 500, min length 1');
     }
     if (strlen($_POST['description']) > 64000) {
-        throw new InvalidArgumentException("Max description is 64000");
+        throw new InvalidArgumentException('Max description is 64000');
     }
     $emailLength = strlen($_POST['email']);
     if ($emailLength > 100 || $emailLength < 4) {
-        throw new InvalidArgumentException("Email min length 4, max length 100");
+        throw new InvalidArgumentException('Email min length 4, max length 100');
     }
     $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
     if (!$email) {
-        throw new InvalidArgumentException("Email invalid");
+        throw new InvalidArgumentException('Email invalid');
     } 
-    if($_POST['domain'] != "ourblog.dev"){
-        throw new InvalidArgumentException("Domain invalid");
+    if($_POST['domain'] != 'ourblog.dev'){
+        throw new InvalidArgumentException('Domain invalid');
     }
 } catch (Exception $e) {
-    exit("Param error!");
+    exit('Param error!');
 }
 require_once __DIR__ . '/db.php';
-$sql = "SELECT id FROM customer WHERE name = ?";
+$sql = 'SELECT id FROM customer WHERE name = ?';
 $stmt = $db->prepare($sql);
 $stmt->execute(array($email));
 $userId = $stmt->fetchColumn();
 if(!$userId){
-    $sql = "INSERT INTO customer( name ) VALUES( ? )";
+    $sql = 'INSERT INTO customer( name ) VALUES( ? )';
     $stmt = $db->prepare($sql);
     $stmt->execute(array($email));
     $userId = $db->lastInsertId();
 }
-$sql = "INSERT INTO ticket(title, description, user, domain) VALUES(?, ?, ?, ?)";
+$sql = 'INSERT INTO ticket(title, description, user, domain) VALUES(?, ?, ?, ?)';
 $stmt = $db->prepare($sql);
 $stmt->execute(array(
     $_POST['title'],
     $_POST['description'],
     $userId,
-    $_POST['domain']
+    'ourblog.dev'
 ));
+echo "success";
