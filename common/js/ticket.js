0f6b0b7905b45ae20fcf07fff6e9321610987d77
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-04-25 15:42:26 +0800

    style(ticket.js):update the quote

--- common/js/ticket.js
+++ common/js/ticket.js
@@ -8,22 +8,22 @@ $(document).ready(function () {
                     '<textarea id="descprition" rows="10" cols="20"></textarea>' +
                     '<br><button type="submit" id="ticket_sub">submit</button>' +
                 '</form>';
-    $(".ticket").append(form);
+    $('.ticket').append(form);
     
-    $(".create_ticket").click(function () {
-        $('.ticket_form').css("display", "inline");
+    $('.create_ticket').click(function () {
+        $('.ticket_form').css('display', 'inline');
     });
     
-    $(".ticket_form").submit(function (e) {
+    $('.ticket_form').submit(function (e) {
         e.preventDefault(); 
-        $.post("http://ticket.dev/ticket_create.php", 
+        $.post('http://ticket.dev/ticket_create.php', 
                 {title: $('input[name=ticket_title]').val(), description: $('#descprition').val(), email: $('#email').text(), domain:location.host}, 
                 function(){ alert('send successfully')}
                 );    
     });
     
-    $(".view_ticket").click(function () {
-        location.href="http://ticket.dev/index.php?email=" +  $('#email').text();
+    $('.view_ticket').click(function () {
+        location.href='http://ticket.dev/index.php?email=' +  $('#email').text();
     });
 });
 
