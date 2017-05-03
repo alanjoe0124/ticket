7f978d3287e6a0a769bc8530d10040219706f370
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-05-03 12:54:58 +0800

    unit_test(Ticket_login):test cs_user login exception and check user info if exists

--- lib/Ticket.php
+++ lib/Ticket.php
@@ -192,4 +192,33 @@ class Ticket {
         return $comments;
     }
 
+    public function login(array $data) {
+        try {
+            $paramArr = array('userName', 'pwd');
+            foreach ($paramArr as $param) {
+                if (!isset($data[$param])) {
+                    throw new InvalidArgumentException("Missing required $param");
+                }
+            }
+            $data['userName'] = trim($data['userName']);
+            $userNameLength = mb_strlen($data['userName'], 'UTF-8');
+            if ($userNameLength > 50 || $userNameLength < 3) {
+                throw new InvalidArgumentException('User name max length 50, min length 3');
+            }
+            $pwdLength = strlen($data['pwd']);
+            if ($pwdLength > 40 || $pwdLength < 5) {
+                throw new InvalidArgumentException('Password max length 40, min length 5');
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
+        $db = Db::getDb();
+        $sql = 'SELECT id, name FROM user WHERE name = ? and pwd = ?';
+        $stmt = $db->prepare($sql);
+        $salt = 'acd806b0-d563-4824-907f-852f8f1003a5';
+        $stmt->execute(array($data['userName'], md5($data['pwd'] . $salt)));
+        $userRow = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $userRow;
+    }
+
 }
