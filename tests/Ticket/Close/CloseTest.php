be6e27ad153938308901649329cc04c825697e32
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-05-19 15:18:28 +0800

    Ticket close test done

---
+++ tests/Ticket/Close/CloseTest.php
@@ -0,0 +1,46 @@
+<?php
+
+class Ticket_CloseTest extends Ticket_Database_TestCase {
+
+    public function getDataSet() {
+        return $this->createArrayDataSet(array(
+                    'customer' => array(
+                        array('id' => 1, 'name' => 'test001@163.com')
+                    ),
+                    'ticket' => array(
+                        array(
+                            'id'            => 1,
+                            'title'         => 'how to write blog?',
+                            'description'   => 'RT. how to write blog?',
+                            'user'          => 1,
+                            'domain'        => 'ourblog.dev',
+                            'status'        => 1
+                        )
+                    )
+        ));
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Customer Email and ticket id not related
+     */
+    public function testCloseUnrelatedTicket() {
+
+        $ticket = new ZendX_Ticket();
+        $ticket->close('test001@163.com', 2);
+    }
+
+    public function testCloseTicket() {
+        $ticket = new ZendX_Ticket();
+        $ticket->close('test001@163.com', 1);
+
+        $expectedDataSet = $this->createArrayDataSet(include __DIR__ . '/expect.php');
+
+        $dataSet = $this->getConnection()->createDataSet(array('customer', 'ticket'));
+        $filterDataSet = new PHPUnit_Extensions_Database_DataSet_DataSetFilter($dataSet);
+        $filterDataSet->setExcludeColumnsForTable('ticket', array('time'));
+
+        $this->assertDataSetsEqual($expectedDataSet, $filterDataSet);
+    }
+
+}
