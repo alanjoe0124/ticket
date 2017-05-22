ee302c453cf9c239baf0878f023251f18e5dc006
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-05-22 08:57:08 +0800

    correct admin module config

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,13 +1,5 @@
 <?php
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
-
-    protected function _initControllerPath() {
-        $front = Zend_Controller_Front::getInstance();
-        $front->setControllerDirectory(array(
-            'default'   => APPLICATION_PATH . '/controllers',
-            'admin'     => APPLICATION_PATH . '/admin/controllers'
-        ));
-    }
-
+ 
 }
