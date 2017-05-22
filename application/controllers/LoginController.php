3f504c2702ad4f51490a9b9b225a8b90ba5c29f7
zhiyuan <zhiyuan@zhiyzh.(none)>
2017-05-22 16:52:14 +0800

    (Login fix):modify some class name and redirect path

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -1,14 +1,15 @@
 <?php
 
-class Admin_LoginController extends Zend_Controller_Action {
+class LoginController extends Zend_Controller_Action
+{
 
-    public function indexAction() {
+    public function indexAction()
+    {
         if ($_POST) {
             try {
-                $session = new Zend_Session_Namespace();
-                ZendX_Csrf::prevent($_SERVER['HTTP_REFERER']);
+                MyLib_Csrf::prevent($_SERVER['HTTP_REFERER']);
                 $auth = Zend_Auth::getInstance();
-                $adapter = new ZendX_Auth($_POST['userName'], $_POST['pwd']);
+                $adapter = new MyLib_Auth($_POST['userName'], $_POST['pwd']);
                 $result = $auth->authenticate($adapter);
             } catch (InvalidArgumentException $e) {
                 exit('Param error');
@@ -16,22 +17,24 @@ class Admin_LoginController extends Zend_Controller_Action {
                 exit('Server error');
             }
             if ($result->isValid()) {
+                $session = new Zend_Session_Namespace();
                 Zend_Session::regenerateId();
                 $session->uid = $result->getIdentity();
                 $session->userName = $adapter->getUserName();
-                $this->redirect('/admin/Manage');
+                $this->redirect('/manage');
                 exit;
             } else {
-                $this->redirect('/admin/Login/index?error=password_failed');
+                $this->redirect('/login/index?error=password_failed');
                 exit;
             }
         }
     }
 
-    public function preDispatch() {
+    public function preDispatch()
+    {
         $auth = Zend_Auth::getInstance();
         if ($auth->hasIdentity()) {
-            $this->redirect('/admin/Manage');
+            $this->redirect('/manage');
         }
     }
 
