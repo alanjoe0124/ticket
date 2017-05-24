6a9f2809c454fa1b44528ef6844b8223bf5d4553
zhiyuan <alanjoe0124@163.com>
2017-05-24 11:15:02 +0800

    ticket.js ok

--- common/js/ticket.js
+++ common/js/ticket.js
@@ -1,33 +1,37 @@
-
 $(document).ready(function () {
-    var form = '<form class="ticket_form" style="display:none">' +
-            '<p>ticket</p>' +
-            '<p>title</p>' +
-            '<input type="text" name="ticket_title">' +
-            '<br>desc<br>' +
-            '<textarea id="descprition" rows="10" cols="20"></textarea>' +
-            '<br><button type="submit" id="ticket_sub">submit</button>' +
-            '</form>';
-    $('.ticket').append(form);
+    var form = '<form id="ticket_form" style="display:none;position:fixed;top:80;">' +
+                    '<p>ticket</p>' +
+                    'title<br>' +
+                    '<input type="text" id="ticket_title"><br>' +
+                    'desc<br>' +
+                    '<textarea id="ticket_description" rows="10" cols="20"></textarea><br>' +
+                    '<button type="submit">submit</button>' +
+               '</form>';
+    $('body').append(form);
 
-    $('.create_ticket').click(function () {
-        $('.ticket_form').css('display', 'inline');
-    });
+    var customerEmail;
 
-    $('.ticket_form').submit(function (e) {
+    $('#ticket_form').submit(function (e) {
         e.preventDefault();
-        $.post('http://ticket.dev/ticket_create.php',
-                {title: $('input[name=ticket_title]').val(), description: $('#descprition').val(), email: $('#email').text(), domain: location.host},
-                function (response) {
-                    if (response === "success") {
-                        alert('send successfully');
-                    }
-                },
-                "text");
+        
+        var postData = {
+            title: $('#ticket_title').val(),
+            description: $('#ticket_description').val(),
+            email: customerEmail,
+            domain: location.host
+        };
+        $.post('http://ticket.dev/ticket_create.php', postData, function (response) {
+            if (response === 'success') {
+                alert('ticket created');
+            }
+        }, 'text');
     });
 
-    $('.view_ticket').click(function () {
-        location.href = 'http://ticket.dev/index.php?email=' + $('#email').text();
-    });
+    window.createTicket = function (email) {
+        $('#ticket_form').show();
+        customerEmail = email;
+    };
+    window.viewMyTickets = function (email) {
+        location.href = 'http://ticket.dev/index.php?email=' + encodeURIComponent(email);
+    }
 });
-
