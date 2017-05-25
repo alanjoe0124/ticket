c0b893013249e8fa82277a3eb42fca65756ccc7c
zhiyuan <alanjoe0124@163.com>
2017-05-25 15:45:51 +0800

    customer前台首页完成

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,18 +1,26 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
+class IndexController extends OurTicket_Action
 {
-
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
     public function indexAction()
     {
-        // action body
+        try {
+            $email = OurTicket_Util::validateEmail(OurTicket_Util::getQuery('email'));
+        } catch (InvalidArgumentException $e) {
+            exit('invalid email');
+        }
+
+        $session = new Zend_Session_Namespace();
+        $db = Zend_Db_Table_Abstract::getDefaultAdapter();
+        if (!isset($session->customerId) || $session->customerEmail != $email) {
+            $customerId = $db->fetchOne('SELECT id FROM customer WHERE email = ?', array($email));
+            if (!$customerId) {
+                $db->insert('customer', array('email'=>$email));
+                $customerId = $db->lastInsertId();
+            }
+            Zend_Session::regenerateId();
+            $session->customerId = $customerId;
+            $session->customerEmail = $email;
+        }
     }
-
-
 }
-
